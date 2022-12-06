f6822cabee4f9dfb64526c54f69790c59c4151f0
HnsmDvl <hnsmdvl@icloud.com>
2022-12-06 17:38:47 -0600

    SPA

---
+++ components/Header.js
@@ -0,0 +1,11 @@
+import html from "html-literal";
+
+export default (state) => html`
+<header>
+    <div class="businessHeadline">
+      <h1>HiddenTreasures</h1>
+      <h2>by Debora Sue</h2></a>
+      <h3>${state.header}</h3>
+    </div>
+  </header>
+  `;
