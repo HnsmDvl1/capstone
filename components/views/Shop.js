3641c39f65144a92434cda0b89ec73bfefd514cf
HnsmDvl <hnsmdvl@icloud.com>
2022-12-28 18:11:26 -0600

    12.28 Update

--- components/views/Shop.js
+++ components/views/Shop.js
@@ -2,28 +2,42 @@ import html from "html-literal";
 
 export default () => html`
   <section id="shop">
-    <!-- <body>
-      <div class="earings">
+    <body>
+      <!-- <div class="necklace">
+        <label for="necklaces">Necklaces</label>
         <img
-          class="imagebox1"
-          src="/assets/earingsVector.jpg"
-          alt="Picture of a
-        pair of earings"
-          width="500"
-          height="500"
+          src="https://fakestoreapi.com/img/71pWzhdJNwL._AC_UL640_QL65_ML3_.jpg"
         />
-        <p>some sort of description goes here.</p>
-      </div>
-      <div class="necklace">
+        <input
+          id="5"
+          title="John Hardy Women's Legends Naga Gold & Silver Dragon Station Chain Bracelet"
+          price="$0.00"
+          description="From our Legends Collection, the Naga was inspired by the mythical water dragon that protects the ocean's pearl. Wear facing inward to be bestowed with love and abundance, or outward for protection."
+          category="jewelery"
+        />
+        <label for="earings">Earings</label>
         <img
-          class="imagebox2"
-          src="/assets/GJvector.jpg"
-          alt="Nice necklace"
-          width="500"
-          height="500"
+          src="https://fakestoreapi.com/img/61sbMiUnoGL._AC_UL640_QL65_ML3_.jpg"
         />
-        <p>some sort of description goes here.</p>
+        <input
+          id="6"
+          title="Solid Gold Petite Micropave"
+          price="$0.00"
+          description="Satisfaction Guaranteed. Return or exchange any order within 30 days.Designed and sold by Hafeez Center in the United States. Satisfaction Guaranteed. Return or exchange any order within 30 days."
+          category="jewelery"
+        />
+        <label for="bracelet">Bracelets</label>
+        <img
+          src="/assets/BaceletVector.jpg"
+        />
+        <input
+          id="7"
+          title="White Gold Plated Princess,
+          price=9.99"
+          description="Classic Created Wedding Engagement Solitaire Diamond Promise Ring for Her. Gifts to spoil your love more for Engagement, Wedding, Anniversary, Valentine's Day..."
+          category="jewelery"
+        /> -->
       </div>
-    </body> -->
+    </body>
   </section>
 `;
