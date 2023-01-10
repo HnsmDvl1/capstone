f61a21bf9b1d25c7dd912af0361d6e35cb5c31ed
HnsmDvl <hnsmdvl@icloud.com>
2023-01-10 17:44:13 -0600

    01.10.23 Update

--- components/views/Home.js
+++ components/views/Home.js
@@ -5,11 +5,10 @@ export default state => html`
     <h1>Hidden Treasures</h1>
     <h2>by Debora Sue</h2>
     <a href="shop" type="button">ENTER SHOP</a>
+    <h3>
+      The weather in ${state.weather.city} is ${state.weather.description}.
+      Temperature is ${state.weather.temp}F, and it feels like
+      ${state.weather.feelsLike}F.
+    </h3>
   </section>
-
-  <h3>
-    The weather in ${state.weather.city} is ${state.weather.description}.
-    Temperature is ${state.weather.temp}F, and it feels like
-    ${state.weather.feelsLike}F.
-  </h3>
 `;
